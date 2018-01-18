6200eba44d3db44d11ad8d9d912503f42336cba0
John Rykken and Masen Matthews <john@>
2018-01-18 11:55:02 -0800

    revise if/then statements

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,26 @@ $(document).ready(function() {
   var inputArray = Array.from(userInput);
   var aySingle = (userInput + "ay");
 
-  inputArray.forEach(function(x){
-    if ((x === "a") || (x === "e") || (x === "i") || (x === "o") || (x === "u"))  {
-        $("#new-sentence").text(aySingle);
-    } else if ((x !== "a") || (x !== "e") || (x !== "i") || (x !== "o") || (x !== "u")) {
-        $("#new-sentence").text(aySingle);
-    }
-    
-  });
-
-  console.log();
+  if (inputArray[0] === "a" || inputArray[0] === "e" || inputArray[0] === "i" || inputArray[0] === "o" || inputArray[0] === "u") {
+    $("#new-sentence").text(aySingle);
+  } else if (inputArray[0] !== "a" || inputArray[0] !== "e" || inputArray[0] !== "i" || inputArray[0] !== "o" || inputArray[0] !=="u") {
+    $("#new-sentence").text(aySingle);
+  };
 
 
+  // inputArray.forEach(function(x){
+  //   if ((x === "a") || (x === "e") || (x === "i") || (x === "o") || (x === "u"))  {
+  //       alert(aySingle);
+  //       $("#new-sentence").text(aySingle);
+  //   } else if ((x !== "a") || (x !== "e") || (x !== "i") || (x !== "o") || (x !== "u")) {
+  //       $("#new-sentence").text(aySingle);
+  //   }
+  //
+  // });
 
-  });
-});
+
+
+
+
+  }); // submit button
+}); // document ready
