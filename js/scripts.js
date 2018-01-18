219fb15dfa4358ce4bfdc08cc481a82aca09714a
John Rykken and Masen Matthews <john@>
2018-01-18 13:45:27 -0800

    complete js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function() {
   $("#form").submit(function(event) {
   event.preventDefault();
@@ -5,27 +7,19 @@ $(document).ready(function() {
   // The Array.from method turns a string into an array
   var inputArray = Array.from(userInput);
   var aySingle = (userInput + "ay");
+  var charLength = inputArray.length;
+  var firstLetter = userInput.slice(0,1);
+  var finalWord = (userInput.slice(1, ) + firstLetter + "ay");
 
   if (inputArray[0] === "a" || inputArray[0] === "e" || inputArray[0] === "i" || inputArray[0] === "o" || inputArray[0] === "u") {
     $("#new-sentence").text(aySingle);
-  } else if (inputArray[0] !== "a" || inputArray[0] !== "e" || inputArray[0] !== "i" || inputArray[0] !== "o" || inputArray[0] !=="u") {
+  } else if ((inputArray[0] !== "a" || inputArray[0] !== "e" || inputArray[0] !== "i" || inputArray[0] !== "o" || inputArray[0] !=="u") && charLength === 1) {
     $("#new-sentence").text(aySingle);
-  };
+  }
 
-
-  // inputArray.forEach(function(x){
-  //   if ((x === "a") || (x === "e") || (x === "i") || (x === "o") || (x === "u"))  {
-  //       alert(aySingle);
-  //       $("#new-sentence").text(aySingle);
-  //   } else if ((x !== "a") || (x !== "e") || (x !== "i") || (x !== "o") || (x !== "u")) {
-  //       $("#new-sentence").text(aySingle);
-  //   }
-  //
-  // });
-
-
-
-
+  else {
+    $("#new-sentence").text(finalWord);
+};
 
   }); // submit button
 }); // document ready
