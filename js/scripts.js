d243b63905d593a6e45c2b0f4abca0950eea592d
John Rykken and Masen Matthews <masen@>
2018-01-18 11:25:24 -0800

    enhance js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
-  var userInput = $("#user-sentence").val();
-  $("#new-sentence").text(userInput);
   event.preventDefault();
+  var userInput = $("#user-sentence").val();
+  // The Array.from method turns a string into an array
+  var inputArray = Array.from(userInput);
+  var aySingle = (userInput + "ay");
+
+  inputArray.forEach(function(x){
+    if ((x === "a") || (x === "e") || (x === "i") || (x === "o") || (x === "u"))  {
+        $("#new-sentence").text(aySingle);
+    } else if (()
+  });
+
+  console.log();
+
+
+
   });
 });
